c126886e3e51620e00725ee6f3acecbd9dd52f5f
Pelile23 <pelile.chintimbwe@gmail.com>
2023-10-14 15:05:46 -0700

    task 0

---
+++ alx-printf_project/main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+int main()
+{
+	printf("pelly and omar printf ALX\n");
+	printf("hello, world");
+
+	printf("\n_______________________\n");
+	
+	printf("ALX printf project c19\n");
+	_printf("hello, world\n");
+
+	return (0);
+}
+
